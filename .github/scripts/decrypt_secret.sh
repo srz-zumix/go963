cffa97c035f4cbf1b2a0a5f3f0ae4ceb2c1e2b00
srz_zumix <zumix.cpp@gmail.com>
2021-01-19 10:52:43 +0900

    set env

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,7 +5,7 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/debug_client_secret.json debug_client_secret.json.gpg
+--output $GO963_SECRETFILE debug_client_secret.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/debug.token debug.token.gpg
+--output $GO963_CACHETOKENFILE debug.token.gpg
